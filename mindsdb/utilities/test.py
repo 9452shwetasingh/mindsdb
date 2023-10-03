695a210859d19fabfa20c8cbbe31207454dcbb40
Max Stepanov <stpmax@yandex.ru>
2023-10-03 17:44:35 +0300

    debug

--- mindsdb/utilities/test.py
+++ mindsdb/utilities/test.py
@@ -15,10 +15,12 @@ def test2(mark):
     print(f'TEST2 {mark} 3')
     task.result()
     print(f'TEST2 {mark} 4')
+    pool.shutdown(wait=True)
+    print(f'TEST2 {mark} 5')
 
 
 if __name__ == "__main__":
-    test2('main')
+    # test2('main')
     context = mp.get_context('spawn')
     p = context.Process(target=test2, args=('proc', ))
     p.start()
